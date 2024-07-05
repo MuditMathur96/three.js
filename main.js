f67896b9a455ae47fbe729f8e360a58c0bfd5708
Mudit Mathur <mudit.manu.mathur@gmail.com>
2024-07-05 18:03:00 +0530

    intial-mesh

--- main.js
+++ main.js
@@ -18,4 +18,21 @@ camera.position.z = 2;
 
 const scene = new THREE.Scene(); 
 
-renderer.render(scene,camera);
+const ico = new THREE.IcosahedronGeometry(1.0,2);
+const mat = new THREE.MeshStandardMaterial({
+    color:"#444444",
+    flatShading:true
+});
+
+const shape = new THREE.Mesh(ico,mat);
+
+scene.add(shape);
+
+function update(t=0){
+    requestAnimationFrame(update);
+    shape.scale.setScalar(Math.cos(t * 0.001)+1.0);
+    renderer.render(scene,camera);
+
+}
+
+update();
